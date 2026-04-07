abfee31ac2543617ff625e3c6d621b3f69ee58aa
guilhermecavs <guipc2019@gmail.com>
2026-04-07 01:34:07 -0300

    feat: add BCB API extraction / adiciona extração da API do BCB

---
+++ src/extract.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+from datetime import datetime,timedelta
+# Dicionário com o nome e código de cada indicador no SGS do Banco Central
+
+SERIES = {
+"selic": 11,
+"cambio_usd":1,
+"ipca":433,
+
+}
+# URL base da API 
+BASE_URL= "https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo}/dados"
+ # Monta a URL com o código da série específica
+def buscar_serie(nome,codigo,data_inicio,data_fim):
+    url= BASE_URL.format(codigo=codigo)
+       # Parâmetros da requisição: formato e período de coleta
+    params = {
+        "formato":"json",
+        "dataInicial":data_inicio,
+        "dataFinal":data_fim,
+        }
+         # Faz a requisição HTTP para a API do Banco Central
+    response = requests.get(url,params= params ,timeout=15)
+    dados = response.json()
+    # Converte o JSON retornado para um DataFrame do pandas
+    return pd.DataFrame(dados)
