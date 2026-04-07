ff838bffcafe8537cc8392bf3f4104919137908e
guilhermecavs <guipc2019@gmail.com>
2026-04-07 01:07:10 -0300

    feat: add database connection / adiciona conexão com o banco

---
+++ src/database.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+def get_engine():
+    url =( f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    )
+    engine = create_engine(url)
+    return engine
